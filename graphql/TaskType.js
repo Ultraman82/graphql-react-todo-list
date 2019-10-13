6ac4db2f8cc85f58562ceada7190e66c5a654fa4
Ultraman82 <exelcior99@gmail.com>
2019-10-13 10:36:38 +0900

    Firstcommit

---
+++ graphql/TaskType.js
@@ -0,0 +1,16 @@
+var GraphQLObjectType = require('graphql').GraphQLObjectType
+var GraphQLNonNull = require('graphql').GraphQLNonNull
+var GraphQLID = require('graphql').GraphQLID
+var GraphQLString = require('graphql').GraphQLString
+
+exports.taskType = new GraphQLObjectType({
+    name: 'task',
+    fields: () => {
+        return {
+            id: {
+                type: new GraphQLNonNull(GraphQLID)
+            },
+            title: { type: GraphQLString }
+        }
+    }
+})
---
+++ server.js
@@ -0,0 +1,33 @@
+
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+const mongoose = require('mongoose')
+// import graphql-express and TaskSchema
+const graphqlExpress = require("express-graphql");
+const taskSchema = require('./graphql/TaskSchema').TaskSchema;
+
+//mongodb+srv://bauhause:bau0099@cluster0-79kmu.mongodb.net/test?retryWrites=true&w=majority
+
+//mongoose.connect("mongodb://bautest:bau0099@ds233288.mlab.com:33288/graphql", { useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect("mongodb+srv://bauhause:bau0099@cluster0-79kmu.mongodb.net/test?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true }
+    , (err) => {
+        if (err) throw err;
+        console.log("connected to mongo atlas")
+    })
+
+
+
+app.set('port', (process.env.PORT || 5000))
+//add the schema to graphql-express 
+app.use('/graphql', graphqlExpress({
+    schema: taskSchema,
+    rootValue: global,
+    graphiql: true
+}));
+
+app.listen(app.get('port'), () => {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
